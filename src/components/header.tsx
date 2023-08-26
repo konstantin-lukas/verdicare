aff9727064e26155478436b50859344f9614bdf4
Konstantin <konstantin_lukas@web.de>
2023-08-26 20:54:55 +0200

    style header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from '../media/verdicare.svg'
+import { HiMiniMagnifyingGlass } from 'react-icons/hi2';
 import './header.scss';
 export default function Header() {
     return (
@@ -8,8 +9,13 @@ export default function Header() {
                 <img src={logo} alt="Verdicare"/>
             </a>
             <div>
-                <input type="text" name="search"/>
-                <span>UGHWAUHGWIU</span>
+                <div className="searchWrapper">
+                    <input type="text" name="search" placeholder="Look for a plant"/>
+                    <HiMiniMagnifyingGlass className="searchIcon"/>
+                </div>
+                <span className="flagWrapper">
+                    <span className="fi fi-gr fis"></span>
+                </span>
             </div>
         </header>
     );
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './main.scss';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Header from './components/header'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <Header/>
   </React.StrictMode>
 );
 
